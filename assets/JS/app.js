3fe209390fd868dda5d5d296509a6d0d1609ed10
Lucyloules <lucia.lagos@gmail.com>
2018-02-22 15:46:30 -0300

    cambio en app.js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  /* Api marvel*/
   let inputText = document.getElementById('input-text');
   let btnSearch = document.getElementById('btn-search');
-  let searchedForText;
 
-
-  /* btnSearch.addEventListener('click', function() {
+  btnSearch.addEventListener('click', function() {
     $('.marvelDate').empty();
     $('.imgMarvel').empty();
-    fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
+    	fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
       .then(function(response) {
         console.log(response);
         return response.json();
@@ -20,26 +17,7 @@ $(document).ready(function() {
           content.push(data.data.results[i].title);
         }
         console.log(content);
-        $('.marvelDate').append('<h5>' + content + '</h5>');
+        $('#marvelDate').append('<h5>' + content + '</h5>');
       });
-  }); */
-  function buscar() {
-    $('.marvelDate').empty();
-    $('.imgMarvel').empty();
-    fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        let content = [];
-        for (let i = 0; i < data.data.results.length; i++) {
-          content.push(data.data.results[i].title);
-        }
-        console.log(content);
-        $('.marvelDate').append('<h5>' + content + '</h5>');
-      });
-  }
+  });
 });
-/* fghjklñ{fghjklñfghjkl   sacarrrrrrrrrr  } */
