55f61bf2375967473e20da786ff26d137751d5b5
Lucyloules <lucia.lagos@gmail.com>
2018-02-22 12:44:25 -0300

    cerrar sesion

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let searchedForText;
 
 
-  btnSearch.addEventListener('click', function() {
+  /* btnSearch.addEventListener('click', function() {
     $('.marvelDate').empty();
     $('.imgMarvel').empty();
     fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
@@ -22,5 +22,23 @@ $(document).ready(function() {
         console.log(content);
         $('.marvelDate').append('<h5>' + content + '</h5>');
       });
-  });
+  }); */
+  function buscar() {
+    $('.marvelDate').empty();
+    $('.imgMarvel').empty();
+    fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
+      .then(function(response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        let content = [];
+        for (let i = 0; i < data.data.results.length; i++) {
+          content.push(data.data.results[i].title);
+        }
+        console.log(content);
+        $('.marvelDate').append('<h5>' + content + '</h5>');
+      });
+  }
 });
--- assets/JS/firebase.js
+++ assets/JS/firebase.js
@@ -1,15 +1,33 @@
-$(document).ready(() => {
+
+/* función que observa la sesion activa de un usuario*/
+function observador() {
+  // Si existe un cambio de usuario, se ejecuta un if y caso contrario ejecuta un else
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
+      // User is signed in.
       $('#main').hide();
       $('#contenido').show();
+      aparece(user); // se envia el parametro user a la funcion aparece
+      console.log('Existe usuario activo');
+      var displayName = user.displayName;
+      var email = user.email;
+      // console.log('Correo verificado: ' + user.emailVerified);
+      // var emailVerified = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
     } else {
+      // No user is signed in.
+      console.log('No existe usuario activo');
       $('#contenido').hide();
-      $('#login-btn').click(ingresar);
+      $('#main').show();
+      $('#btnLogIn').click(ingresar);
       $('#btnSignIn').click(registrar);
     }
-  }); // firebase
-});
+  });
+}
+observador(); // se ejecuta cuando se carga la documento
+
 
 // Registro de nuevos usuarios*/
 function registrar() {
@@ -34,18 +52,75 @@ function registrar() {
 
 // Ingreso usuarios o logueo
 function ingresar() {
-  // console.log('diste click en Ingresar');
+  console.log('diste click en Ingresar');
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
-  // console.log(email);
-  // console.log(password);
+  console.log(email);
+  console.log(password);
   firebase.auth().signInWithEmailAndPassword(email, password)
     .catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // console.log(errorCode);
-      // console.log(errorMessage);
+      console.log(errorCode);
+      console.log(errorMessage);
       alert(error.code);
     });
 };
+/* Funcion para desloguearse */
+function cerrar() {
+  // se envia el parametro user a la funcion aparece
+  firebase.auth().signOut() // Cierra sesion con firebase, toma 2 parametros then y catch
+    .then(function() { // (respuesta positiva)
+      // Sign-out successful.
+      console.log('Sesión cerrada');
+      alert('Su sesion ha cerrado');
+    }).catch(function(error) {// (respuesta negativa)/error : parametro
+      // An error happened.
+      console.log(error);
+    });
+}
+/* $('#btnLogOut').click(cerrar); //   clase, ya que hay 2 btns para desloguearse movil y desktop---- > fallo de esta forma
+$('#btnLogOut2').click(cerrar); */
+
+/* funcion para usuarios logueados */
+function aparece(user) { // parametro user recibido desde el observador
+  var user = user;
+  var contenido = document.getElementById('contenido');
+  /* Comillas especiales nos permiten hacer template donde podemos escribir codigo html en el codigo javascript*/
+  contenido.innerHTML = `
+      <section>
+      <nav class="red darken-1">
+        <div class="nav-wrapper container">
+          <a href="#" class="brand-logo">
+            <img class="logo logo-nav" src="assets/IMG/marvel.png" alt="Marvel">
+          </a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a class="user" href="#">Bienvenido, ${user.email}</a></li> 
+              <li><a onclick="cerrar()" href="#">Log out</a></li>    
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+              <li><a class="user" href="#">Bienvenido, ${user.email}</a></li>
+              <li><a onclick="cerrar()" href="#">Log out</a></li>
+            </ul>
+        </div>
+      </nav>
+      <div class="row">
+        <div class="col-s-12">
+          <input id="input-Text" type="text" name="" value="">
+          <button id="btn-search" class="waves-effect waves-light btn grey darken-4 white-text"type="button" name="button">Buscar</button>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="row">
+        <div class="col-lg-12 imgMarvel"></div>
+        <div class="col-lg-12 col-lg-offset-4 text-center">
+          <div class="marvelDate grey darken-4"></div>
+        </div>
+      </div>
+    </section>
+      `;
+}
+
